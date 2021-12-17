4ea732188ec7fa4966c90799f6c567735dc282a1
AmiraliPak <a4amirali@hotmail.com>
2021-12-18 02:18:46 +0330

    add simple player movement

---
+++ Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotationSpeed;
+    float MoveAmount { get => Time.deltaTime * moveSpeed; }
+    float RotationAmount { get => Time.deltaTime * rotationSpeed; }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        var verticalMovement = Input.GetAxis("Vertical") * MoveAmount;
+        var horizontalMovement = Input.GetAxis("Horizontal") * MoveAmount;
+        var rotationMovement = Input.GetAxis("Mouse X") * RotationAmount;
+        transform.Rotate(new Vector3(-verticalMovement, rotationMovement, horizontalMovement));
+    }
+}
