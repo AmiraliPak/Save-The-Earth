b5a4a745aa903c89c7eabf1ecfc5f00ea036c0d3
AmiraliPak <a4amirali@hotmail.com>
2022-01-13 05:08:07 +0330

    feat(weapons): add MissileWeapon

--- Unity Project/Assets/Scripts/Missile.cs
+++ Unity Project/Assets/Scripts/Missile.cs
@@ -8,13 +8,7 @@ public class Missile : Projectile
     float MoveAmount { get => Time.fixedDeltaTime * moveSpeed; }
     Transform target;
     bool isLocked = false;
-    Rigidbody rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
         if(isLocked)
@@ -29,6 +23,12 @@ public class Missile : Projectile
 
     public void LockOn(Transform target)
     {
+        // if(target == null)
+        // {
+        //     Debug.Log("Missile: Target not locked");
+        //     Deactivate();
+        //     return;
+        // }
         this.target = target;
         isLocked = true;
     }
