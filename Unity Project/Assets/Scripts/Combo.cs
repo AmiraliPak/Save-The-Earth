d6cf86329baac54ccb4bc80328f0371ddfb95426
AmiraliPak <a4amirali@hotmail.com>
2022-01-20 02:16:06 +0330

    add earth shield combo

---
+++ Unity Project/Assets/Scripts/Combo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICombo 
+{
+    void ActivateCombo();
+}
+
+public class EarthShieldCombo : ICombo
+{
+    public static GameObject EarthShield{ set; private get; }
+    public void ActivateCombo()
+    {
+        EarthShield.SetActive(false);
+        EarthShield.SetActive(true);
+    }
+}
